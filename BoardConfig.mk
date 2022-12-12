0fab7d51c46973e994bcea206d27f1465839c34a
Android HQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2022-12-12 15:36:34 +0300

    zero6: Enable CPUSets

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,10 @@ TARGET_SCREEN_DENSITY := 480
 TARGET_SCREEN_HEIGHT := 2260
 TARGET_SCREEN_WIDTH := 1080
 
+# Enable CPUSets
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 androidboot.selinux=permissive
